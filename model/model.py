6a121e157629c3ef6e4fd804ebe572a240e154c6
zanpakutou <lienlienlien369@gmail.com>
2021-12-07 16:36:04 +0700

    refine

--- model/model.py
+++ model/model.py
@@ -2,11 +2,11 @@ import pickle
 import pandas as pd
 import cv2
 
-from variable import _dir, highland_dir
+from variable import _dir, pretrain_dir
 
-df = pd.read_csv(highland_dir + "label_highland.csv")
+df = pd.read_csv(pretrain_dir + "label_highland.csv")
 
 # Final ML model with full datas
-ridge_path = highland_dir + "ridge_highland.sav"
+ridge_path = pretrain_dir + "ridge_highland.sav"
 
 loaded_model = pickle.load(open(ridge_path, 'rb'))
--- model/variable.py
+++ model/variable.py
@@ -1,6 +1,5 @@
 _dir = "./Dataset/receipt_dataset/"
-highland_dir = "./Dataset/receipt_highland/"
-phuclong_dir = "./Dataset/receipt_phuclong/"
+pretrain_dir = "./pretrain/"
 segmenation_blur_params = [5,9,15]
 segmenation_erode_params = [(3,11),(11,3),(25,1),(1,25)]
 image_shape = (600,600)
